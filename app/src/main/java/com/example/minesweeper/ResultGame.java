34158128de7f57ce4bbb77bcc67a6d995ec23cb4
Khanh Pham <khanhpha@usc.edu>
2022-09-16 20:44:48 -0700

    9draft

--- app/src/main/java/com/example/minesweeper/ResultGame.java
+++ app/src/main/java/com/example/minesweeper/ResultGame.java
@@ -1,6 +1,7 @@
 package com.example.minesweeper;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,10 +16,13 @@ public class ResultGame extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra("com.minesweeper.MESSAGE");
+        String message = intent.getStringExtra("com.example.minesweeper.MESSAGE");
+        //message = message;
+        System.out.println(message);
 
         TextView textView = (TextView) findViewById(R.id.resultMessage);
         textView.setText(message);
+        textView.setTextColor(Color.BLACK);
     }
 
     public void backToMain(View view) {
